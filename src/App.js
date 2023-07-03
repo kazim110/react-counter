488580fa8ef4dc170b64e89bf34edf02153dce1e
kazim <kazim.mohammadi110@gmail.com>
2023-07-03 09:23:54 +0430

    modifying code for javascript best practice

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count,setCount] = useState(0);
-  
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>You Clicked {count} time.</h1>
-        <br/>
-        <button onClick={()=>{
-          setCount(count+1)
-        }}>Click Me!</button>
+        <br />
+        <button onClick={() => setCount(count + 1)}>
+          Click Me!
+        </button>
       </header>
     </div>
   );
