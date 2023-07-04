f82d6c47b3fd38c631b8e148a569188a2cb7a637
kazim <kazim.mohammadi110@gmail.com>
2023-07-04 09:08:47 +0430

    Using useEffect hook in counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(()=>{
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>You Clicked {count} time.</h1>
         <br />
-        <button onClick={() => setCount(count + 1)}>
+        <button onClick={() => setCount(count + 10)}>
           Click Me!
         </button>
       </header>
